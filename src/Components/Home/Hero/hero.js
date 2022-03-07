9b03f1b1f6cdc32c982100c36630ce8ca90f8dad
samapth-ops <sampathkaali002@gmail.com>
2022-03-07 20:24:56 +0530

    gradient button added in hero section

--- src/Components/Home/Hero/hero.js
+++ src/Components/Home/Hero/hero.js
@@ -1,14 +1,20 @@
 import "./hero.css";
+import { Link } from "react-router-dom";
 const Hero = () => {
     return ( 
         <div className="hero">
-            <div className="hero__title">Synergy</div>
+            
             <div className="cube"></div>
             <div className="cube"></div>
             <div className="cube"></div>
             <div className="cube"></div>
             <div className="cube"></div>
             <div className="cube"></div>
+            <div className="hero__title">
+                <p>Synergy</p>
+                <Link to="/register"><button class="btn-hover color-3">Register Now</button></Link>
+            </div>
+            
         </div>
      );
 }
