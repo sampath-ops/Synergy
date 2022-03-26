9a3e6d2a1e6fa268ba761c20c615df895d175eeb
samapth-ops <sampathkaali002@gmail.com>
2022-03-26 15:01:35 +0530

    memory page gallery added

--- src/Components/PreviousSynergy/PreviousSynergy.js
+++ src/Components/PreviousSynergy/PreviousSynergy.js
@@ -1,12 +1,24 @@
 import NavBar from "../Navbar/navbar";
 import PreviousHero from "./PreviousHero";
 import MomentCards from "./MomentCards";
+import symbol from "../../images/Memories/synergy_symbol.jpg";
+import group1 from "../../images/Memories/pic3.jpg";
+import "./PreviousSynergy.css";
 const PreviousSynergy = () => {
     return ( 
         <NavBar className="second_nav">
             <div className="previous_synergy">
-                <PreviousHero/>
-                <MomentCards/>
+            `   <MomentCards/>
+                <h2>Synergy 2020 Gallery</h2>
+                <div className="grid_layout">
+                    <PreviousHero/> 
+                    <div className="side_layout">
+                        <img src={symbol} alt="synergy" />
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi, aspernatur?</p>
+                        <img src={group1} alt="group" />
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi, aspernatur?</p>
+                    </div> 
+                </div>    
             </div>
         </NavBar>
      );
