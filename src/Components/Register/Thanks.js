e060782d99cb85ffac1b7a5e6304587203c442d2
samapth-ops <sampathkaali002@gmail.com>
2022-04-05 21:35:41 +0530

    changes added

--- src/Components/Register/Thanks.js
+++ src/Components/Register/Thanks.js
@@ -5,7 +5,7 @@ const Thanks = (props) => {
         <div className="thanks">
             <h2>Thanks for Registering</h2>
             <p>Your id : <b>{props.studentAbstract.id}</b></p>
-            <p>Check your mail {props.studentAbstract.email} to have reference of your id which will be used for on spot payment.</p>
+            <p>Check your mail {props.studentAbstract.email} for reference of your ID which will be used for<b>on spot payment.</b></p>
         </div>
     );
 }
