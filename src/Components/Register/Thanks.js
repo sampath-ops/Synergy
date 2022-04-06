76158d8ba811911ed14a6f3596d18ee6a5a62ea1
sampath <57784373+sampath-ops@users.noreply.github.com>
2022-04-06 09:43:16 +0530

    Registration fee added in thanks page

--- src/Components/Register/Thanks.js
+++ src/Components/Register/Thanks.js
@@ -6,7 +6,8 @@ const Thanks = (props) => {
             <h2>Thanks for Registering</h2>
             <p>Your id : <b>{props.studentAbstract.id}</b></p>
             <p>Check your mail {props.studentAbstract.email} for reference of your ID which will be used for <br /> <b> on spot payment.</b></p>
-        </div>
+            <p>Registration Fee: &#8377;250</p>      
+ </div>
     );
 }
  
