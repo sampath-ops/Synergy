3a2dbb6cf8d6af10e2faac41e15bd5a99c5c4ab4
samapth-ops <sampathkaali002@gmail.com>
2022-04-07 13:57:23 +0530

    SITE COMPLETED V1

--- src/Components/Register/Thanks.js
+++ src/Components/Register/Thanks.js
@@ -4,7 +4,7 @@ const Thanks = (props) => {
     return (  
         <div className="thanks">
             <h2>Thanks for Registering</h2>
-            <p>Your id : <b>{props.studentAbstract.id}</b></p>
+            <p>Your id : SYNERGY-<b>{props.studentAbstract.id}</b></p>
             <p>Check your mail {props.studentAbstract.email} for reference of your ID which will be used for <br /> <b> on spot payment.</b></p>
             <p style={{fontWeight:"bold"}}>Registration Fee: <span style={{fontFamily: 'FontAwesome'}}>&#x20b9;</span>250</p>      
  </div>
